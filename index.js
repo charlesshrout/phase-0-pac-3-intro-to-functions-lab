9b15e094ddc3ae7771a1a4799fd97f1353ff7073
Charles <charlie.david@me.com>
2022-09-09 13:19:02 -0600

    commit tree 31be852265af2ce63a9f642142a482f848ebb789

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string)
+{return string.toUpperCase()}
+
+function whisper(string)
+{return string.toLowerCase()}
+
+function logShout(string)
+{console.log(string.toUpperCase())}
+
+function logWhisper(string)
+{console.log(string.toLowerCase())}
+
+const string = "Let\'s have dinner together!"
+function sayHiToHeadphonedRoommate(string)
+{switch (string) 
+    {case string.toLowerCase(): return "I can\'t hear you!"
+    case string.toUpperCase(): return "YES INDEED!"
+    default : return "I would love to!"
+}}
